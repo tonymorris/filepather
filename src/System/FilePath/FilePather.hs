a589d2f07310651fdbe2777e168e7e65ecb88893
Tony Morris <tmorris@tmorris.net>
2014-09-02 16:17:06 +1000

    start again

--- src/System/FilePath/FilePather.hs
+++ src/System/FilePath/FilePather.hs
@@ -1,9 +1,1 @@
-module System.FilePath.FilePather(module X) where
-
-import System.FilePath.FilePather.FileType as X
-import System.FilePath.FilePather.RecursePredicate as X
-import System.FilePath.FilePather.FilterPredicate as X
-import System.FilePath.FilePather.FilePathPredicate as X
-import System.FilePath.FilePather.Find as X
-import System.FilePath.FilePather.LiftI as X
-
+module System.FilePath.FilePather where
