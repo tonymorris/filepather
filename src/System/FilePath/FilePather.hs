0339203e512d2fcc5b701dd6e0593a741a167ef7
Tony Morris <tmorris@tmorris.net>
2014-09-02 20:41:17 +1000

    tidy

--- src/System/FilePath/FilePather.hs
+++ src/System/FilePath/FilePather.hs
@@ -2,106 +2,139 @@ module System.FilePath.FilePather where
 
 import Control.Applicative(Applicative(pure, (<*>)), liftA2)
 import Control.Category(Category((.)))
+import Control.Lens.Iso(Iso, iso)
 import Control.Monad(Monad((>>=), return), liftM)
 import Control.Monad.Trans.Class(MonadTrans(lift))
+import Data.Bool(Bool)
 import Data.Functor(Functor(fmap))
 import Data.Functor.Apply(Apply((<.>)), liftF2)
 import Data.Functor.Bind(Bind((>>-)))
 import Data.Functor.Bind.Trans(BindTrans(liftB))
+import Data.Functor.Identity(Identity(Identity, runIdentity))
+import Data.String(String)
 import System.FilePath(FilePath)
+import qualified System.FilePath as SP
 
-newtype FilePathState f a =
-  FilePathState (FilePath -> f (FilePath, a))
+newtype FilePathStateT f a =
+  FilePathStateT (FilePath -> f (FilePath, a))
 
-instance Functor f => Functor (FilePathState f) where
-  fmap f (FilePathState k) =
-    FilePathState (fmap (fmap f) . k)
+instance Functor f => Functor (FilePathStateT f) where
+  fmap f (FilePathStateT k) =
+    FilePathStateT (fmap (fmap f) . k)
 
-instance Bind f => Apply (FilePathState f) where
-  FilePathState f <.> FilePathState a =
-    FilePathState (\p -> f p >>- \(q, g) -> fmap (fmap g) (a q))
+instance Bind f => Apply (FilePathStateT f) where
+  FilePathStateT f <.> FilePathStateT a =
+    FilePathStateT (\p -> f p >>- \(q, g) -> fmap (fmap g) (a q))
 
-instance (Bind f, Applicative f) => Applicative (FilePathState f) where
-  FilePathState f <*> FilePathState a =
-    FilePathState (\p -> f p >>- \(q, g) -> fmap (fmap g) (a q))
+instance (Bind f, Applicative f) => Applicative (FilePathStateT f) where
+  FilePathStateT f <*> FilePathStateT a =
+    FilePathStateT (\p -> f p >>- \(q, g) -> fmap (fmap g) (a q))
   pure a =
-    FilePathState (\p -> pure (p, a))
+    FilePathStateT (\p -> pure (p, a))
 
-instance Bind f => Bind (FilePathState f) where
-  FilePathState k >>- f =
-    FilePathState (\p -> k p >>- \(q, a) -> let FilePathState s = f a in s q)
+instance Bind f => Bind (FilePathStateT f) where
+  FilePathStateT k >>- f =
+    FilePathStateT (\p -> k p >>- \(q, a) -> let FilePathStateT s = f a in s q)
 
-instance Monad f => Monad (FilePathState f) where
-  FilePathState k >>= f =
-    FilePathState (\p -> k p >>= \(q, a) -> let FilePathState s = f a in s q)
+instance Monad f => Monad (FilePathStateT f) where
+  FilePathStateT k >>= f =
+    FilePathStateT (\p -> k p >>= \(q, a) -> let FilePathStateT s = f a in s q)
   return a =
-    FilePathState (\p -> return (p, a))
+    FilePathStateT (\p -> return (p, a))
 
-instance MonadTrans FilePathState where
+instance MonadTrans FilePathStateT where
   lift a =
-    FilePathState (\p -> liftM ((,) p) a)
+    FilePathStateT (\p -> liftM ((,) p) a)
 
-instance BindTrans FilePathState where
+instance BindTrans FilePathStateT where
   liftB a =
-    FilePathState (\p -> fmap ((,) p) a)
+    FilePathStateT (\p -> fmap ((,) p) a)
 
-instance Applicative f => GetFilePath (FilePathState f) where
+instance Applicative f => GetFilePath (FilePathStateT f) where
   getFilePath =
-    FilePathState (\p -> pure (p, p))
+    FilePathStateT (\p -> pure (p, p))
 
-instance Applicative f => PutFilePath (FilePathState f) where
+instance Applicative f => PutFilePath (FilePathStateT f) where
   putFilePath p =
-    FilePathState (\_ -> pure (p, ()))
+    FilePathStateT (\_ -> pure (p, ()))
+  modifyFilePath k =
+    FilePathStateT (\p -> pure (k p, ()))
+
+type FilePathState a =
+  FilePathStateT Identity a
+
+filePathStateTIso ::
+  Iso (FilePathStateT f a) (FilePathStateT f b) (FilePath -> f (FilePath, a)) (FilePath -> f (FilePath, b))
+filePathStateTIso =
+  iso (\(FilePathStateT k) -> k) FilePathStateT
+
+filePathStateIso ::
+  Iso (FilePathState a) (FilePathState b) (FilePath -> (FilePath, a)) (FilePath -> (FilePath, b))
+filePathStateIso =
+  iso (\(FilePathStateT k) -> runIdentity . k) (\k -> FilePathStateT (Identity . k))
 
 ----
 
-newtype FilePathReader f a =
-  FilePathReader (FilePath -> f a)
+newtype FilePathReaderT f a =
+  FilePathReaderT (FilePath -> f a)
 
-instance Functor f => Functor (FilePathReader f) where
-  fmap f (FilePathReader k) =
-    FilePathReader (fmap f . k)
+instance Functor f => Functor (FilePathReaderT f) where
+  fmap f (FilePathReaderT k) =
+    FilePathReaderT (fmap f . k)
 
-instance Apply f => Apply (FilePathReader f) where
-  FilePathReader f <.> FilePathReader a =
-    FilePathReader (liftF2 (<.>) f a)
+instance Apply f => Apply (FilePathReaderT f) where
+  FilePathReaderT f <.> FilePathReaderT a =
+    FilePathReaderT (liftF2 (<.>) f a)
   
-instance Applicative f => Applicative (FilePathReader f) where
-  FilePathReader f <*> FilePathReader a =
-    FilePathReader (liftA2 (<*>) f a)
+instance Applicative f => Applicative (FilePathReaderT f) where
+  FilePathReaderT f <*> FilePathReaderT a =
+    FilePathReaderT (liftA2 (<*>) f a)
   pure =
-    FilePathReader . pure . pure
+    FilePathReaderT . pure . pure
 
-instance Bind f => Bind (FilePathReader f) where
-  FilePathReader k >>- f =
-    FilePathReader (\p -> k p >>- \a -> let FilePathReader r = f a in r p)
+instance Bind f => Bind (FilePathReaderT f) where
+  FilePathReaderT k >>- f =
+    FilePathReaderT (\p -> k p >>- \a -> let FilePathReaderT r = f a in r p)
 
-instance Monad f => Monad (FilePathReader f) where
-  FilePathReader k >>= f =
-    FilePathReader (\p -> k p >>= \a -> let FilePathReader r = f a in r p)
+instance Monad f => Monad (FilePathReaderT f) where
+  FilePathReaderT k >>= f =
+    FilePathReaderT (\p -> k p >>= \a -> let FilePathReaderT r = f a in r p)
   return =
-    FilePathReader . return . return
+    FilePathReaderT . return . return
 
-instance MonadTrans FilePathReader where
+instance MonadTrans FilePathReaderT where
   lift =
-    FilePathReader . pure
+    FilePathReaderT . pure
 
-instance BindTrans FilePathReader where
+instance BindTrans FilePathReaderT where
   liftB =
-    FilePathReader . pure
+    FilePathReaderT . pure
 
-instance Applicative f => GetFilePath (FilePathReader f) where
+instance Applicative f => GetFilePath (FilePathReaderT f) where
   getFilePath =
-    FilePathReader pure
+    FilePathReaderT pure
+
+type FilePathReader a =
+  FilePathReaderT Identity a
+
+filePathReaderTIso ::
+  Iso (FilePathReaderT f a) (FilePathReaderT f b) (FilePath -> f a) (FilePath -> f b)
+filePathReaderTIso =
+  iso (\(FilePathReaderT k) -> k) FilePathReaderT
+
+filePathReaderIso ::
+  Iso (FilePathReader a) (FilePathReader b) (FilePath -> a) (FilePath -> b)
+filePathReaderIso =
+  iso (\(FilePathReaderT k) -> runIdentity . k) (\k -> FilePathReaderT (Identity . k))
 
 ----
 
 readState ::
   Functor f =>
-  FilePathReader f a
-  -> FilePathState f a
-readState (FilePathReader k) =
-  FilePathState (\p -> fmap (\a -> (p, a)) (k p))
+  FilePathReaderT f a
+  -> FilePathStateT f a
+readState (FilePathReaderT k) =
+  FilePathStateT (\p -> fmap (\a -> (p, a)) (k p))
 
 ----
 
@@ -111,7 +144,104 @@ class Functor f => GetFilePath f where
 
 ----
 
-class Functor f => PutFilePath f where
+class GetFilePath f => PutFilePath f where
   putFilePath ::
     FilePath
     -> f ()
+  modifyFilePath ::
+    (FilePath -> FilePath)
+    -> f ()    
+
+----
+
+splitExtension ::
+  GetFilePath f =>
+  f (String, String)
+splitExtension =
+  fmap SP.splitExtension getFilePath 
+
+takeExtension ::
+  GetFilePath f =>
+  f String
+takeExtension =
+  fmap SP.takeExtension getFilePath   
+
+replaceExtension ::
+  PutFilePath f =>
+  String
+  -> f ()
+replaceExtension ext = 
+  modifyFilePath (`SP.replaceExtension` ext)
+
+dropExtension ::
+  PutFilePath f =>
+  f ()
+dropExtension =
+  modifyFilePath SP.dropExtension
+
+addExtension ::
+  PutFilePath f =>
+  String
+  -> f ()
+addExtension ext = 
+  modifyFilePath (`SP.addExtension` ext)
+
+hasExtension ::
+  GetFilePath f =>
+  f Bool
+hasExtension =
+  fmap SP.hasExtension getFilePath 
+
+splitExtensions ::
+  GetFilePath f =>
+  f (FilePath, String)
+splitExtensions =
+  fmap SP.splitExtensions getFilePath 
+
+dropExtensions ::
+  PutFilePath f =>
+  f ()
+dropExtensions =
+  modifyFilePath SP.dropExtensions
+
+takeExtensions ::
+  GetFilePath f =>
+  f String
+takeExtensions =
+  fmap SP.takeExtensions getFilePath 
+
+splitDrive ::
+  GetFilePath f =>
+  f (FilePath, FilePath)
+splitDrive =
+  fmap SP.splitDrive getFilePath 
+
+joinDrive ::
+  (GetFilePath f, PutFilePath g) =>
+  f (g ())
+joinDrive =
+  fmap (modifyFilePath . SP.joinDrive) getFilePath
+
+takeDrive ::
+  PutFilePath f =>
+  f ()
+takeDrive =
+  modifyFilePath SP.takeDrive
+  
+hasDrive ::
+  GetFilePath f =>
+  f Bool
+hasDrive =
+  fmap SP.hasDrive getFilePath 
+
+dropDrive ::
+  PutFilePath f =>
+  f ()
+dropDrive =
+  modifyFilePath SP.dropDrive
+  
+isDrive ::
+  GetFilePath f =>
+  f Bool
+isDrive =
+  fmap SP.isDrive getFilePath 
